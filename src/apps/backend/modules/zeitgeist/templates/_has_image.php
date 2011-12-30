aa3489d66b2c7948143071c36fdf4a98c26f60a6
Tristan Rivoallan <tristan@rivoallan.net>
2011-12-30 16:38:30 +0100

    Minor tweaks in zeitgeist admin.

---
+++ src/apps/backend/modules/zeitgeist/templates/_has_image.php
@@ -0,0 +1,9 @@
+<?php if ($lum_zeitgeist->image): ?>
+
+<img src="<?php echo image_path('/sfTwitterBootstrapPlugin/images/button-check.png') ?>" />
+
+<?php else: ?>
+
+<img src="<?php echo image_path('/sfTwitterBootstrapPlugin/images/button-cross.png') ?>" />
+
+<?php endif; ?>
