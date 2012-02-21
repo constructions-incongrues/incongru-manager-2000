b06ae689f519d162a342210a6e18c571280fe6fe
Tristan Rivoallan <tristan@rivoallan.net>
2012-02-21 11:58:33 +0100

    [preview] Added preview link

---
+++ src/apps/backend/modules/zeitgeist/templates/_preview.php
@@ -0,0 +1,8 @@
+<div class="clearfix sf_admin_form_row">
+	<div>
+		<label>Prévisualisation</label>
+		<div class="input">
+			<a href="http://zeitgeist.musiques-incongrues.net/<?php echo $form->getObject()->zeitgeistid ?>?preview=true" target="_blank">Prévisualiser</a>
+		</div>
+	</div>
+</div>
