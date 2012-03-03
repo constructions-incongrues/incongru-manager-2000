0f2e42a15e74b2e68adafe211a2046210b2537c8
Tristan Rivoallan <tristan@rivoallan.net>
2012-03-03 16:51:10 +0100

    [data] unified videos

--- src/apps/backend/modules/zeitgeist/templates/_data.php
+++ src/apps/backend/modules/zeitgeist/templates/_data.php
@@ -4,8 +4,7 @@ $zeitgeist = $form->getObject();
 $dateTimeStart = DateTime::createFromFormat('Y-m-d', $zeitgeist->datestart);
 $dateTimeEnd = DateTime::createFromFormat('Y-m-d', $zeitgeist->dateend);
 $urlImages = sprintf('http://data.musiques-incongrues.net/collections/links/segments/images/get?contributed_at=[%sT00:00:00Z%%20TO%%20%sT00:00:00Z]&viewmode=gallery', $dateTimeStart->format('Y-m-d'), $dateTimeEnd->format('Y-m-d'));
-$urlYoutube = sprintf('http://data.musiques-incongrues.net/collections/links/segments/youtube/get?contributed_at=[%sT00:00:00Z%%20TO%%20%sT00:00:00Z]&viewmode=gallery', $dateTimeStart->format('Y-m-d'), $dateTimeEnd->format('Y-m-d'));
-$urlVimeo = sprintf('http://data.musiques-incongrues.net/collections/links/segments/all/get?contributed_at=[%sT00:00:00Z%%20TO%%20%sT00:00:00Z]&domain_parent=vimeo.com&viewmode=gallery', $dateTimeStart->format('Y-m-d'), $dateTimeEnd->format('Y-m-d'));
+$urlVideos = sprintf('http://data.musiques-incongrues.net/collections/links/segments/all/get?contributed_at=[%sT00:00:00Z%%20TO%%20%sT00:00:00Z]&domain_parent=vimeo.com%%20OR%%20youtube.com%%20OR%%20dailymotion.com&viewmode=gallery', $dateTimeStart->format('Y-m-d'), $dateTimeEnd->format('Y-m-d'));
 $urlAll = sprintf('http://data.musiques-incongrues.net/collections/links/segments/all/get?contributed_at=[%sT00:00:00Z%%20TO%%20%sT00:00:00Z]&viewmode=gallery', $dateTimeStart->format('Y-m-d'), $dateTimeEnd->format('Y-m-d'));
 ?>
 
@@ -15,8 +14,7 @@ $urlAll = sprintf('http://data.musiques-incongrues.net/collections/links/segment
 		<div class="input">
 			<ul>
 				<li><a target="_blank" href="<?php echo $urlImages ?>">Les images</a></li>
-				<li><a target="_blank" href="<?php echo $urlYoutube ?>">Les vidéos youtube</a></li>
-				<li><a target="_blank" href="<?php echo $urlVimeo ?>">Les vidéos vimeo</a></li>
+				<li><a target="_blank" href="<?php echo $urlVideos ?>">Les vidéos</a></li>
 				<li><a target="_blank" href="<?php echo $urlAll ?>">Tous les liens</a></li>
 			</ul>
 		</div>
